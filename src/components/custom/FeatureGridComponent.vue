b434b47b4b14904d752ffe5de9a0ba347122c817
christianokuru <okuruchristian@gmail.com>
2025-02-10 20:35:59 +0100

    Finished the left hand side of the feature section

--- src/components/custom/FeatureGridComponent.vue
+++ src/components/custom/FeatureGridComponent.vue
@@ -17,11 +17,11 @@ const props = defineProps({
 
 <template>
     <div class="flex items-center w-[210px]">
-        <div :class="[bgColor, 'w-[40px] h-[40px] rounded-[999px] p-[10px] bg-[#FFFFFF1A]] mr-[16px]']">
+        <div :class="[bgColor, 'w-[53px] h-[40px] rounded-[999px] p-[10px] bg-[#FFFFFF1A]]']">
             <img :src="icon" :alt="text" class="w-[20px] h-[20px] object-contain" />
         </div>
         <div>
-            <h1 class="font-500 text-16px leading-[22.4px] text-white">{{ text }}</h1>
+            <h1 class="font-500 text-16px leading-[22.4px] text-white ml-[16px]">{{ text }}</h1>
         </div>
     </div>
 </template>
--- src/components/custom/FeatureSectionComponent.vue
+++ src/components/custom/FeatureSectionComponent.vue
@@ -1,53 +1,54 @@
 <script setup>
-import FeatureGridComponent from '@/components/custom/FeatureGridComponent.vue'
-import chain from '@/assets/images/chain.svg'
-import thunder from '@/assets/images/thunder.svg'
-import settings from '@/assets/images/settings.svg'
-import { ref } from 'vue'
+import FeatureGridComponent from "@/components/custom/FeatureGridComponent.vue";
+import chain from "@/assets/images/chain.svg";
+import thunder from "@/assets/images/thunder.svg";
+import settings from "@/assets/images/settings.svg";
+import { ref } from "vue";
 
 const features = ref([
-    { 
-        text: 'Designed for crypto trading platforms',
-        icon: chain,
-        bgColor: 'bg-[#FFFFFF1A]'
-    },
-    { 
-        text: 'Designed for crypto trading platforms',
-        icon: thunder,
-        bgColor: 'bg-[#FFFFFF1A]'
-    },
-    { 
-        text: 'Designed for crypto trading platforms',
-        icon: settings,
-        bgColor: 'bg-[#FFFFFF1A]'
-    }
-])
-
+  {
+    text: "Designed for crypto trading platforms",
+    icon: chain,
+    bgColor: "bg-[#FFFFFF1A]",
+  },
+  {
+    text: "Designed for crypto trading platforms",
+    icon: thunder,
+    bgColor: "bg-[#FFFFFF1A]",
+  },
+  {
+    text: "Designed for crypto trading platforms",
+    icon: settings,
+    bgColor: "bg-[#FFFFFF1A]",
+  },
+]);
 </script>
 
 <template>
-    <div>
-        <!-- left div -->
-        <div>
-            <div class="w-[588px] mb-[40px]">
-                <p class="font-400 text-18px leading-[21.6px] mb-[12px]">Why choose <span class="text-primary">crypgo</span></p>
-                <h1 class="text-white font-600 text-[40px] leading-[48px]">Features of the crypto framer mobile application</h1>
-            </div>
-            <!-- grid section -->
-            <div class="grid grid-cols-2 gap-4">
-                <div>
-                    <feature-grid-component 
-                    v-for="(feature, index) in features"
-                    :key="index"
-                    :text="feature.text"
-                    :icon="feature.icon"
-                    :bg-color="feature.bgColor"
-                    />
-                </div>
-            </div>
-        </div>
-        
-        <!-- right div -->
-        <div></div>
+  <div>
+    <!-- left div -->
+    <div class="w-[588px]">
+      <div class="mb-[40px]">
+        <p class="font-400 text-18px leading-[21.6px] mb-[12px]">
+          Why choose <span class="text-primary">crypgo</span>
+        </p>
+        <h1 class="text-white font-600 text-[40px] leading-[48px]">
+          Features of the crypto framer mobile application
+        </h1>
+      </div>
+      <!-- grid section -->
+      <div class="grid grid-cols-2 gap-y-[42px]">
+        <feature-grid-component
+          v-for="(feature, index) in features"
+          :key="index"
+          :text="feature.text"
+          :icon="feature.icon"
+          :bg-color="feature.bgColor"
+        />
+      </div>
     </div>
+
+    <!-- right div -->
+    <div></div>
+  </div>
 </template>
