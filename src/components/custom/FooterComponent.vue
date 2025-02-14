ae108313b10c5d036418069928ee0ca8fe8346e1
christianokuru <okuruchristian@gmail.com>
2025-02-15 00:25:57 +0100

    started the footer

--- src/components/custom/FooterComponent.vue
+++ src/components/custom/FooterComponent.vue
@@ -1,9 +1,35 @@
 <script setup>
-
+import LogoComponent from '@/components/custom/LogoComponent.vue'
+import SocialMediaComponent from '@/components/custom/SocialMediaComponent.vue'
+import fb from '@/assets/images/socials/facebook.svg'
+import twitter from '@/assets/images/socials/twitter.svg'
+import instagram from '@/assets/images/socials/'
 </script>
 
 <template>
-    <footer class=" text-off-white text-center p-4">
-        <p>&copy; 2021 Crypgo</p>
+    <footer class="border py-[88.5px]">
+        <!-- top -->
+        <div class="flex items-center w-[486px] border">
+            <!-- left -->
+            <div>
+                <div>
+                    <div class="mb-[24px]">
+                        <logo-component />
+                    </div>
+                    <p>Transform your crypto business with Crypgo <br/>Framer, a template for startups and blockchain services.</p>
+                </div>
+                <!-- social icons -->
+                <div></div>
+            </div>
+            <!-- right -->
+            <div></div>
+        </div>
+        <!-- bottom -->
+        <div></div>
     </footer>
 </template>
+
+
+
+
+<!-- <p>&copy; 2021 Crypgo</p> -->
---
+++ src/components/custom/SocialMediaComponent.vue
@@ -0,0 +1,24 @@
+<script setup>
+const props = defineProps({
+    bgColor: {
+        type: String,
+    },
+    icon: {
+        type: String,
+    },
+    iconColor: {
+        type: String,
+    },
+    altText: {
+        type: String,
+    }
+})
+</script>
+
+<template>
+<div :class="[bgColor, 'w-[38px] rounded-[999px] p-[9px]']">
+    <div class="">
+        <img :src="icon" :alt="altText" class="w-[20px] h-[20px]">
+    </div>
+</div>
+</template>
