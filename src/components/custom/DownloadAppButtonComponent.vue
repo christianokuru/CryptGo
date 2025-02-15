77c17a5dd5cf21d49e09aeae2c20a9fa85446871
christianokuru <okuruchristian@gmail.com>
2025-02-15 03:41:11 +0100

    still working on the store buttons

--- src/components/custom/DownloadAppButtonComponent.vue
+++ src/components/custom/DownloadAppButtonComponent.vue
@@ -1,5 +1,21 @@
 <script setup>
-
+const props = defineProps({
+    bgColor: String,
+    icon: String,
+    smallText: String,
+    largeText: String,
+    altText: String,
+})
 </script>
 
-<template></template>
+<template>
+    <div class="p-[3.38px] rounded-[6.77px] w-[132.85px]">
+        <div class="flex items-center">
+            <img :src="icon" :alt="altText" class="mr-[7.72px]">
+            <div>
+                <p class="text-16px font-500 mb-[44.46px]">{{ smallText }}</p>
+                <h1 class="text-[24px] font-600">{{ largeText }}</h1>
+            </div>
+        </div>
+    </div>
+</template>
