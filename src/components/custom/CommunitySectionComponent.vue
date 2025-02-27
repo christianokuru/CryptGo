acd85c817938b770b39580b5baf5dc7b438b06de
christianokuru <okuruchristian@gmail.com>
2025-02-27 20:17:39 +0100

    minor updates

--- src/components/custom/CommunitySectionComponent.vue
+++ src/components/custom/CommunitySectionComponent.vue
@@ -7,7 +7,7 @@ import community from '@/assets/images/community.svg'
         <div class="mt-[72px] mb-[40px]">
             <p class="font-400 text-18px leading-[21.6px] mb-[12px]">Always by <span class="text-primary">your side</span></p>
             <div>
-                <h1 class="text-center font-600 text-[40px] leading-[48px] mb-[12px]">Be the first to use our Crypgo!</h1>
+                <h1 class="text-center font-600 text-[40px] leading-[48px] mb-[12px]">Be the first to use Crypgo!</h1>
                 <p class="font-400 text-[16px] leading-[22.4px] text-[#FFFFFF99]">Get faster, safer, more affordable cloud object storage with <br/>no centeral point of failure.</p>
             </div>
         </div>
